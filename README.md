d673ee1fea63f28f9e979694171a0fd37c0f1d87
zhangmin <530023877@qq.com>
2018-04-18 21:56:28 +0800

    add global structure

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# yiminhuixcx
+
---
+++ pages/home/index.js
@@ -0,0 +1,12 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    imgUrls: [],
+    indicatorDots: true,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
+  },
+})
